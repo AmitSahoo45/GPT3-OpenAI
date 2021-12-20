8c3fac5bb6a600b2db167df759efb6888b2d5875
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2021-12-20 22:31:11 +0530

    Initial Commit

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Article } from './article/Article'
+export { default as Brand } from './brand/Brand'
+export { default as CTA } from './cta/CTA'
+export { default as Feature } from './feature/Feature'
+export { default as Navbar } from './navbar/Navbar'
+// this is also a useful feature as we export all our components into a js file and import it to the app.js file
+// rather than doing all the unnecessary stuff there within the app.js file and making
+//  it untidy
